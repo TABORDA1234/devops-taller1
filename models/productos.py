c817cfc1e6ba7c596cb90b0560c149a24a070275
TABORDA1234 <luicarlostabordaflorez@gmail.com>
2026-08-19 16:49:44 -0500

    clase producto

---
+++ models/productos.py
@@ -0,0 +1,8 @@
+#clase productos
+
+class producto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    precio = db.Column(db.Float, nullable=False)
+    descripcion = db.Column(db.String(100), nullable=False)
+    cantidad = db.Column(db.Integer, nullable=False)
